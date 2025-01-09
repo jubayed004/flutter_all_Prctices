6c2b96dbbbb432e4031ef0e2116b00497673ab4b
jubayed004 <mohammadjubayed.islam97@gmail.com>
2025-01-09 14:53:08 +0600

    git main some input

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         // Leraning Flutter
-        //Thank you
+        //Thank you git main
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
